8fef6e6a5328edff3c66c295020c62e37e0a3bb1
Max <max.yablokov02@yandex.ru>
2023-10-29 15:31:57 +0300

    unused methods have been removed

--- include/FuzzySet.h
+++ include/FuzzySet.h
@@ -49,14 +49,6 @@ namespace FuzzyLogic
         set_name_ = name;
     }
 
-    //template<typename T>
-    //FuzzySet<T>::FuzzySet(std::wstring name, const std::initializer_list<T>& values, const std::vector<std::wstring>& terms)
-    //{
-    //    for (size_t i = 0; i < values.size(); ++i)
-    //        set_[terms[i]] = values[i];
-    //    set_name_ = name;
-    //}
-
     template<typename T>
     FuzzySet<T>::FuzzySet(std::wstring name, const boost::numeric::ublas::vector<T>& values, const std::initializer_list<std::wstring>& terms)
     {
@@ -131,11 +123,6 @@ namespace FuzzyLogic
 
         return out;
     }
-   
-    //template<typename U>
-    //inline U FuzzySet<U>::operator[](std::wstring term)
-    //{
-    //    return set_[term];
-    //}
+
 }
 
